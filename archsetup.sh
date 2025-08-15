757ae6df203e1f7fe6f631fef95397dedc955868
FatihTheDev <130474375+FatihTheDev@users.noreply.github.com>
2025-08-15 13:33:45 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -73,6 +73,9 @@ pacman-key --lsign-key 3056513887B78AEB
 pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
+echo "Refreshing repositories..."
+pacman -Sy
+
 if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
     cat >> /etc/pacman.conf <<'EOF'
 
