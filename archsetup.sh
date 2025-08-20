3f2d5ddfe8a0449ec559422f86b7dd75df423b8b
FatihTheDev <130474375+FatihTheDev@users.noreply.github.com>
2025-08-20 17:27:19 +0200

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -50,7 +50,7 @@ EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable --now reflector.timer
-sudo systemctl enable --now reflector.service
+# sudo systemctl enable --now reflector.service
 
 ### 3. Add Chaotic AUR ###
 echo "Adding Chaotic AUR..."
