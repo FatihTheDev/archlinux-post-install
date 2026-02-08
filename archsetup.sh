bae6347eb526f7640eff74a49cd31d55a082a2f4
FatihTheDev <130474375+FatihTheDev@users.noreply.github.com>
2026-02-08 12:50:14 +0000

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -108,14 +108,13 @@ echo "Enabling parallel downloads and parallel compilation..."
 sudo sed -i 's/^#\s*\(ParallelDownloads\s*=\s*[0-9]*\)/\1/' /etc/pacman.conf
 # Uncommenting MAKEFLAGS to use number of threads available on device in /etc/makepkg.conf
 threads=$(nproc --all)
-sudo awk -v threads="$threads" '
-/^#\s*MAKEFLAGS=/ { sub(/#.*/, "MAKEFLAGS=\"-j" threads "\""); found=1 }
-/^MAKEFLAGS=/ { sub(/=.*/, "=\"-j" threads "\""); found=1 }
-{ print }
-END {
-    if (!found) print "MAKEFLAGS=\"-j" threads "\""
-}
-' /etc/makepkg.conf > /tmp/makepkg.conf && sudo mv /tmp/makepkg.conf /etc/makepkg.conf 
+if grep -q '^#\s*MAKEFLAGS=' /etc/makepkg.conf; then
+    sudo sed -i "s/^#\s*MAKEFLAGS=.*/MAKEFLAGS=\"-j$threads\"/" /etc/makepkg.conf
+elif grep -q '^MAKEFLAGS=' /etc/makepkg.conf; then
+    sudo sed -i "s/^MAKEFLAGS=.*/MAKEFLAGS=\"-j$threads\"/" /etc/makepkg.conf
+else
+    echo "MAKEFLAGS=\"-j$threads\"" | sudo tee -a /etc/makepkg.conf
+fi 
 
 # Uncommenting IgnorePkg in /etc/pacman.conf to make pin and unpin aliases work properly
 sudo sed -i 's/^#\s*IgnorePkg\s*=/IgnorePkg =/' /etc/pacman.conf
